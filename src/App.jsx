7a4b9b2faed27bd86c4c4b905222c96543b48870
CalebDK <calebymish@outlook.com>
2024-07-29 17:35:04 -0500

    Haciendo dinamico el carrito de compras y detectando si esta vacio

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,49 @@ import {db} from './data/db'
 function App() {
  
     //asi definimos el state 
+    //estado de la data -> inicia con la bd local
     const [data, setData]  =useState(db)
+
+    //estado del carrito de compras -> inicia con un arreglo vacio
+    const [cart, setCart] = useState([])
     
+
+    function addToCart(item){
+       const itemExists = cart.findIndex( (guitar) => guitar.id === item.id)
+      if(itemExists >= 0){ //existe en el carrito
+        const updateCart = [...cart]  //sacamos una copia del carrito
+        updateCart[itemExists].quantity++  //sacamos la cantidad del carrito
+        setCart(updateCart)  //actualizamos el carrito
+      }
+      else{
+        item.quantity = 1
+        setCart([...cart, item])
+      }
+
+       
+    }
+
+
 
   return (
     <>
 
-    <Header />
+    <Header 
+      cart={cart}
+    />
 
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            {data.map(() => (
-                  <Guitar />
+            {data.map((guitar) => (
+                  <Guitar 
+                    key={guitar.id}
+                    guitar={guitar}
+                    setCart={setCart}
+                    addToCart={addToCart}
+                  />
             ))}
            
         </div>
